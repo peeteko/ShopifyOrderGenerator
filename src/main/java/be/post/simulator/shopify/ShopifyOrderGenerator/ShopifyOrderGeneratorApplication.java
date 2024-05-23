bace9367e846b882bafd2af5af8adf4c27a081cf
peeteko <koen.peeters@bpost.be>
2024-05-23 08:09:21 +0200

    initial version

---
+++ src/main/java/be/post/simulator/shopify/ShopifyOrderGenerator/ShopifyOrderGeneratorApplication.java
@@ -0,0 +1,13 @@
+package be.post.simulator.shopify.ShopifyOrderGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopifyOrderGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopifyOrderGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/post/simulator/shopify/ShopifyOrderGenerator/ShopifyOrderGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package be.post.simulator.shopify.ShopifyOrderGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopifyOrderGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
